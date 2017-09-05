be88683e01db7adcc5edf8480d1d5d6c6c749b1d
qiudong <qiu_dong@suixinpay.com>
2017-09-05 16:16:43 +0800

    增加sass依赖

--- app/pages/user/login/Login.jsx
+++ app/pages/user/login/Login.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import log from './Login.scss'
 
 class Login extends React.Component{
 
     render(){
         return (
 
+                <div className="container">
+                    <div className="text">sfdsfdsf</div>
 
-            <div>sfdsfdsf</div>
+                </div>
+
         )
     }
 
