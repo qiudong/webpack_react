d84944bbcbac2e934227eba48a302ef2fc79b0fd
qiudong <qiu_dong@suixinpay.com>
2017-09-06 16:25:53 +0800

    增加组件测试路由

---
+++ app/pages/user/loan/Loan.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+class Loan extends React.Component{
+
+    render(){
+        return (
+
+            <h1>
+                贷款
+            </h1>
+
+        )
+    }
+
+}
+export default Loan;
---
+++ app/pages/user/login/Register.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+class Register extends React.Component{
+
+    render(){
+        return (
+
+            <h1>
+                注册
+            </h1>
+
+        )
+    }
+
+}
+export default Register;
